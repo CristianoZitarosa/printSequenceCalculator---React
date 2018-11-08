a559b410b248791ba1905c2de29e9dd8422accaa
CristianoZitarosa <36627397+CristianoZitarosa@users.noreply.github.com>
2018-11-08 18:53:15 +0100

    Copy the original index.html in the App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,32 @@ import '../style/App.css';
 class App extends Component {
   render() {
     return (
-      <p>Hello!</p>
+      <div>
+
+        <header>
+          <h1>Print Sequence Calculator</h1>
+        </header>
+
+        <div id="container">
+          <form id="form">
+            <input type="number" id="page" min="1" step="1" placeholder="Number of pages"/>
+            <input type="submit" id="submit" value="Submit" disabled="disabled"/>
+            <input type="button" id="reset" value="Reset" disabled/>
+            <input type="button" id="help" value=">> Help <<"/>
+            <div>
+              <input type="checkbox" id="select" value="select" disabled/>
+              <label htmlFor="select">Select an interval</label>
+            </div>
+            <div id="parameters">
+              <input type="number" id="minPage" min="1" step="1" disabled placeholder="First page"/>
+              <input type="number" id="maxPage" min="1" step="1" disabled placeholder="Last page"/>
+            </div>
+          </form>
+          <div id="helpTxt"></div>
+          <div id="result"></div>
+        </div>
+
+      </div>
     );
   }
 }
