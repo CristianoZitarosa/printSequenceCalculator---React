7610b1202458b074d5003ad892352ddb2a90895c
CristianoZitarosa <36627397+CristianoZitarosa@users.noreply.github.com>
2018-11-08 20:19:23 +0100

    Create the component Help and set the Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import '../style/App.css';
+import { Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import Help from './Help';
 
 class App extends Component {
   render() {
@@ -9,27 +12,32 @@ class App extends Component {
         <header>
           <h1>Print Sequence Calculator</h1>
         </header>
-
-        <div id="container">
-          <form id="form">
-            <input type="number" id="page" min="1" step="1" placeholder="Number of pages"/>
-            <input type="submit" id="submit" value="Submit" disabled="disabled"/>
-            <input type="button" id="reset" value="Reset" disabled/>
-            <input type="button" id="help" value=">> Help <<"/>
-            <div>
-              <input type="checkbox" id="select" value="select" disabled/>
-              <label htmlFor="select">Select an interval</label>
+        <Route exact path='/' render={() => (
+          <div id="container">
+            <form id="form">
+              <input type="number" id="page" min="1" step="1" placeholder="Number of pages"/>
+              <input type="submit" id="submit" value="Submit" disabled="disabled"/>
+              <input type="button" id="reset" value="Reset" disabled/>
+              <Link to="/help">
+                <input type="button" id="help" value=">> Help <<"/>
+              </Link>
+              <div>
+                <input type="checkbox" id="select" value="select" disabled/>
+                <label htmlFor="select">Select an interval</label>
+              </div>
+              <div id="parameters">
+                <input type="number" id="minPage" min="1" step="1" disabled placeholder="First page"/>
+                <input type="number" id="maxPage" min="1" step="1" disabled placeholder="Last page"/>
+              </div>
+            </form>
+            <div id="result"></div>
             </div>
-            <div id="parameters">
-              <input type="number" id="minPage" min="1" step="1" disabled placeholder="First page"/>
-              <input type="number" id="maxPage" min="1" step="1" disabled placeholder="Last page"/>
-            </div>
-          </form>
-          <div id="helpTxt"></div>
-          <div id="result"></div>
-        </div>
+        )}/>
 
+      <div id="helpPage">
+        <Route path='/help' component={Help}/></div>
       </div>
+
     );
   }
 }
---
+++ src/components/Help.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import '../style/App.css';
+
+class Help extends Component {
+  render() {
+    return (
+        <div className="message">
+
+          <p>
+            This app, is designed to help the user who needs to print a document on both sides of one sheets or more, but he does not own an automatic duplex printer and or the software managing the document is not capable to separately print even/odd pages.
+          </p>
+          <p>
+            Whithout this app the user needs to manually insert a sequence of even/odd pages in the print window. This, for larger documents could really be a trouble, causing mistakes and waste of time and paper if the sequence is wrong.
+          </p>
+          <p>
+            With this app the user needs to just provide the document's number of pages, or an interval of pages (whithin that number). The app automatically provides both the even and odd sequences, ready for a copy and paste and the number of sheets needed for the operation.
+          </p>
+          <p><strong>Example</strong></p>
+          <p>Let say we have a document, 50 pages long.</p>
+          <p>If we have to print all the pages, we just insert 50 in the box "Number of pages" and click "Submit" to receive the result.</p>
+          <p>
+            If we have to print an interval, let say from page 14 to page 37, we insert 50 in the box as said before, then we select the checkbox "Select an interval". The 2 box "First page" and "Last page" will be active now, so we can insert 14 as first page and 37 as last. Lastly we have to click "Submit" to receive the result.
+          </p>
+          <p><strong>Note:</strong></p>
+          <ol>
+            <li>
+              As soon the app is loaded, only the box to insert the number of pages and the help button are available. Other elements are disabled.
+            </li>
+            <li>
+              After a first positive number is provided in the box, other elements will be available. The user will be able to submit the number, to select the checkbox to provide an interval or just reset the app.
+            </li>
+            <li>
+              Once the result is shown, the user can only reset the App for a new sequence.
+            </li>
+          </ol>
+
+      </div>
+    );
+  }
+}
+
+export default Help;
